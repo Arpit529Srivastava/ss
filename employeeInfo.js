cc4d8f193bcd948ea8aabacb8b8c54c2b2b1406d
arpit529srivastava <arpitsrivastava529@gmail.com>
2026-08-18 22:07:09 +0530

    test2
    Signed-off-by: arpit529srivastava <arpitsrivastava529@gmail.com>

---
+++ employeeInfo.js
@@ -0,0 +1,10 @@
+const employeeName = "Ravi Kumar";
+const age = 28;
+const isPermanent = true;
+const skills = ["JavaScript", "HTML", "CSS"];
+
+console.log("Employee Information:");
+console.log("Name: " + employeeName);
+console.log("Age: " + age);
+console.log("Permanent Employee: " + (isPermanent ? "Yes" : "No"));
+console.log("Skills: " + skills.join(", "));
