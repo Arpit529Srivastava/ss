2496321602d4f082e357509bec1e23eae028ea0b
arpit529srivastava <arpitsrivastava529@gmail.com>
2026-08-18 22:11:38 +0530

    test5
    Signed-off-by: arpit529srivastava <arpitsrivastava529@gmail.com>

--- workoutPlan.js
+++ workoutPlan.js
@@ -1,26 +1,32 @@
-function getWorkoutOfTheDay(day) {
-  switch (day) {
-    case 1:
-      return "Chest Day";
-    case 2:
-      return "Back Day";
-    case 3:
-      return "Leg Day";
-    case 4:
-      return "Shoulder Day";
-    case 5:
-      return "Arm Day";
-    case 6:
-      return "Cardio Day";
-    case 7:
-      return "Rest Day";
-    default:
-      return "Invalid day";
-  }
-}
-
+let workout;
 const day = 7;
-const workout = getWorkoutOfTheDay(day);
+
+switch (day) {
+  case 1:
+    workout = "Chest Day";
+    break;
+  case 2:
+    workout = "Back Day";
+    break;
+  case 3:
+    workout = "Leg Day";
+    break;
+  case 4:
+    workout = "Shoulder Day";
+    break;
+  case 5:
+    workout = "Arm Day";
+    break;
+  case 6:
+    workout = "Cardio Day";
+    break;
+  case 7:
+    workout = "Rest Day";
+    break;
+  default:
+    workout = "Invalid day";
+    break;
+}
 
 if (workout === "Invalid day") {
   console.log("Invalid day. Please enter a number between 1 and 7.");
